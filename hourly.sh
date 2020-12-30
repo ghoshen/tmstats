99b429957633f270655765cb7a1eccae58378322
Gur Hoshen <ghoshen1@gmail.com>
2020-12-29 17:18:12 -0800

    parameterize the reports directory

--- hourly.sh
+++ hourly.sh
@@ -9,11 +9,11 @@ cd "$SCRIPTPATH"
 ./dotraining101.sh
 
 # Update contest and training pages
-(cd "$workdir";$SCRIPTPATH/makecontestpage.py && isreal && cp contestschedule.html ~/files/reports/)
-(cd "$workdir";$SCRIPTPATH/maketrainingpage.py && isreal && cp trainingschedule.html ~/files/reports/)
+(cd "$workdir";$SCRIPTPATH/makecontestpage.py && isreal && cp contestschedule.html ${reportdir}/)
+(cd "$workdir";$SCRIPTPATH/maketrainingpage.py && isreal && cp trainingschedule.html ${reportdir}/)
 
 # Update open house results
-(cd "$workdir";$SCRIPTPATH/openhouse.py && isreal && cp openhouseclubs.html ~/files/reports/)
+(cd "$workdir";$SCRIPTPATH/openhouse.py && isreal && cp openhouseclubs.html ${reportdir}/)
 
 # Copy info from Dropbox
 (cd "$workdir"; isreal && $SCRIPTPATH/copywebfiles.py)
